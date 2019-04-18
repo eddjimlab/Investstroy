0fb1939272e201f625c91b79c12768c164dba760
Eduard <eddjimlab@gmail.com>
2019-04-18 15:58:14 +0300

    add calculator

--- js/main.js
+++ js/main.js
@@ -34,3 +34,42 @@ $(document).ready(function () {
             sidebarMenu.toggleClass('left-side-bar_menu__active');
     });
 });
+
+var elem = document.querySelector('.calc-range');
+var init = new Powerange(elem, {
+    min: 100000,
+    max: 3000000,
+    start: 100000,
+    hideRange: true,
+    step: 100000,
+});
+var per,
+    month,
+    result,
+    total,
+    monthly;
+var money = +$('.calc-range').val();
+
+$('input[name="programs"]').on('change', function (event) {
+    month = +$(this).attr('data-month');
+    per = +$(this).attr('data-per');
+    result = Math.round(per / 12 * month * money);
+    total = result + money;
+    monthly = parseInt(result / 12);
+    $('#total').text(total.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));
+    $('.calc-monthly span').text(monthly.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));
+
+});
+
+$('.calc-range').on('change', function (event) {
+    $('.calc-summ_invest_num span').text($(this).val().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));
+    var radio = $('input[name="programs"]:checked');
+    money = +$(this).val();
+    month = +$(radio).attr('data-month');
+    per = +$(radio).attr('data-per');
+    result = Math.round(per / 12 * month * money);
+    total = result + money;
+    monthly = parseInt(result / 12);
+    $('#total').text(total.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));
+    $('.calc-monthly span').text(monthly.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));
+});
